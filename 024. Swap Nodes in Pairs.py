c9db25c41483c5628a587e8b31302fcdbcff8d93
Vicky <126154136+vickyxiz@users.noreply.github.com>
2024-07-30 19:13:07 +0530

    Update 024. Swap Nodes in Pairs.py

--- 024. Swap Nodes in Pairs.py
+++ 024. Swap Nodes in Pairs.py
@@ -2,7 +2,7 @@
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
-#         self.next = next
+#         self.next = next 
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if head is None or head.next is None:
