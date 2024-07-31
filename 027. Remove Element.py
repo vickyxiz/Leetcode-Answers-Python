fc1e651aae1627efe0d6b88e28037a2ac5e6ec06
Vicky <126154136+vickyxiz@users.noreply.github.com>
2024-07-31 19:01:39 +0530

    Update 027. Remove Element.py

--- 027. Remove Element.py
+++ 027. Remove Element.py
@@ -1,7 +1,7 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         k = 0
-        for x in nums:
+        for x in nums: 
             if x != val:
                 nums[k] = x
                 k += 1
