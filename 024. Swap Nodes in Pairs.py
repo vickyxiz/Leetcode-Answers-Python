a3d126a1516a1d9b40e42b451bd5e6ba569f2172
Vicky <126154136+vickyxiz@users.noreply.github.com>
2024-07-31 18:51:37 +0530

    Update 024. Swap Nodes in Pairs.py

--- 024. Swap Nodes in Pairs.py
+++ 024. Swap Nodes in Pairs.py
@@ -1,7 +1,7 @@
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
-#         self.val = val4
+#         self.val = val4q
 #         self.next = next 
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
